46691d5f14e1af933f980a055f64967f2bd5355e
LA <pyth.la.on@gmail.com>
2025-10-20 14:48:29 +0800

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -20,8 +20,8 @@ SELECT
 ROW_NUMBER() OVER(ORDER BY cst_id) as customer_key
 , ci.cst_id AS customer_id
 , ci.cst_key AS customer_number
-, ci.cst_firstname AS firstname
-, ci.cst_lastname AS lastname
+, ci.cst_firstname AS first_name
+, ci.cst_lastname AS last_name
 , cl.cntry AS country
 , ci.cst_marital_status AS marital_status
 , CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
